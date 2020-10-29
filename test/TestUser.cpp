023f224cbd126798c5de15a297b3387795749b1b
Don Castillo <don@tangle.ca>
2020-10-29 01:34:10 -0600

    removed comment

--- test/TestUser.cpp
+++ test/TestUser.cpp
@@ -14,20 +14,3 @@ TEST(TestUser, setting_and_getting_name) {
 
     delete jane;
 }
-
-
-//TEST(TestComment, setting_and_getting_commenter) {
-//    // jane as original commenter
-//    User* jane = new User("Jane");
-//    Comment* myComment = new Comment(jane, "sample comment");
-//    EXPECT_EQ(myComment->getCommenter()->getName(), "Jane");
-//
-//    // change commenter to Bob
-//    User* bob = new User("Bob");
-//    myComment->setCommenter(bob);
-//    EXPECT_EQ(myComment->getCommenter()->getName(), "Bob");
-//
-//    delete myComment;
-//    delete jane;
-//    delete bob;
-//}
