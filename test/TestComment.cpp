85ac39c63b0c5a88a0d5e64572964364f4e2e889
Don Castillo <don@tangle.ca>
2020-10-29 09:47:21 -0600

    fixed test coment

--- test/TestComment.cpp
+++ test/TestComment.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 TEST(TestComment, comment_id) {
-    Comment::counter = 0;
+    unsigned int Comment::counter = 0;
     User* jane = new User("Jane");
     Comment* comment1 = new Comment(jane, "This is comment 1");
     Comment* comment2 = new Comment(jane, "This is comment 2");
