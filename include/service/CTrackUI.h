88fd1b3da2c2998915a6491cef3b1ba803057b2b
Don Castillo <don.castillo@uleth.ca>
2020-11-22 22:49:44 -0700

    fixed style in CTrackUI.h

--- include/service/CTrackUI.h
+++ include/service/CTrackUI.h
@@ -5,16 +5,18 @@
 
 class CTrackUI {
  public:
-   void static println(std::string message);
-   void static print(std::string message);
-   void static printTitle(std::string title);
-   void static printRow(std::string left, std::string right);
-   std::string static formatRow(std::string left, std::string right);
-   std::string static formatIDRow(unsigned int id, std::string right);
-   std::string static formatStringRow(std::string left, std::string right); 
-   static const int LINE_WIDTH = 35;
-   static const char TITLE_FILL = '=';
-   static const char LINE_FILL = '.';
+    static void println(std::string message);
+    static void print(std::string message);
+    static void printTitle(std::string title);
+    static void printRow(std::string left, std::string right);
+    static std::string formatRow(std::string left, std::string right);
+    static std::string formatIDRow(unsigned int id, std::string right);
+    static std::string formatStringRow(std::string left, std::string right);
+
+ protected:
+    const int LINE_WIDTH = 35;
+    const char TITLE_FILL = '=';
+    const char LINE_FILL = '.';
 };
 
 #endif // CTRACKUI_H_INCLUDED
