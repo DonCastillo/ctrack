e73056361ea0a51d54c596f8d26960383120b5ca
Riley <riley.weaselfat@uleth.ca>
2020-11-28 23:46:48 -0700

    Added setComment Test

--- test/TestComment.cpp
+++ test/TestComment.cpp
@@ -9,9 +9,9 @@ TEST(TestComment, getID) {
     User* Bob = new User(0, "Bob");
     Comment* comment1 = new Comment(3, Bob, "Bobs Testing Comment");
     Comment* comment2 = new Comment(15, Bob, "Bob's second Test");
-    Comment* comment3 = new Comment(4000, Bob, "Bob's second Test");
-    Comment* comment4 = new Comment(12, Bob, "Bob's second Test");
-    Comment* comment5 = new Comment(100, Bob, "Bob's second Test");
+    Comment* comment3 = new Comment(4000, Bob, "Bob's asdf Test");
+    Comment* comment4 = new Comment(12, Bob, "Bob's lasdjf Test");
+    Comment* comment5 = new Comment(100, Bob, "Bob's asdfwegddaf");
 
     EXPECT_EQ(comment1->getID(), 3);
     EXPECT_EQ(comment2->getID(), 15);
@@ -19,3 +19,25 @@ TEST(TestComment, getID) {
     EXPECT_EQ(comment4->getID(), 12);
     EXPECT_EQ(comment5->getID(), 100);
 }
+
+TEST(TestComment, setComment) {
+    User* John = new User(1, "John");
+    Comment* comment1 = new Comment(1, John, "Set Test");
+
+    std::string newComment1 = "abc dhe eiru aldkei";
+    comment1->setComment(newComment1);
+    EXPECT_EQ(comment1->getComment(), newComment1);
+
+    std::string newComment2 = "Software engineering";
+    comment1->setComment(newComment2);
+    EXPECT_EQ(comment1->getComment(), newComment2);
+
+    std::string newComment3 = "Comment test";
+    comment1->setComment(newComment3);
+    EXPECT_EQ(comment1->getComment(), newComment3);
+
+    std::string newComment4 = "I'm a new comment";
+    comment1->setComment(newComment4);
+    EXPECT_EQ(comment1->getComment(), newComment4);
+}
+
